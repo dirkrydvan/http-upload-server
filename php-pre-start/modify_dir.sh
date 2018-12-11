84ee810825e5d3d56acdb6bc478e9700fa7a293b
Dirk Rydvan <36986019+dirkrydvan@users.noreply.github.com>
2018-12-11 16:49:51 +0100

    Änderung .htaccess Path

--- php-pre-start/modify_dir.sh
+++ php-pre-start/modify_dir.sh
@@ -1,4 +1,4 @@
 # Erzeuge tmp Pfad zum Upload
 #mkdir -p ${APP_ROOT}/src/files
 #chmod 777 ${APP_ROOT}/src/files
-ln -s ../configmap/htaccess .htaccess
+ln -s ${APP_ROOT}/src/configmap/htaccess ${APP_ROOT}/src/.htaccess
