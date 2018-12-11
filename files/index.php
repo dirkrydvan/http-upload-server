945d29b885325678df87f9ba1aa56f45a1595af6
Dirk Rydvan <36986019+dirkrydvan@users.noreply.github.com>
2018-12-11 15:15:44 +0100

    Fehler bei download

--- files/index.php
+++ files/index.php
@@ -34,7 +34,7 @@ $default_timezone = 'Etc/UTC'; // UTC
 $root_path = $_SERVER['DOCUMENT_ROOT'] . '/files';
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
-$root_url = '';
+$root_url = 'files';
 // Server hostname. Can set manually if wrong
 $http_host = $_SERVER['HTTP_HOST'];
 // input encoding for iconv
