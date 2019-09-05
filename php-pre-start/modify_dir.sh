e02ca7d179cf1b4a200da27f224167013536f4f2
Dirk Rydvan <36986019+dirkrydvan@users.noreply.github.com>
2019-09-05 14:32:40 +0200

    Update modify_dir.sh

--- php-pre-start/modify_dir.sh
+++ php-pre-start/modify_dir.sh
@@ -2,3 +2,4 @@
 #mkdir -p ${APP_ROOT}/src/files
 #chmod 777 ${APP_ROOT}/src/files
 ln -s ${APP_ROOT}/src/configmap/htaccess ${APP_ROOT}/src/.htaccess
+ln -s ${APP_ROOT}/src/configmap/htaccess ${APP_ROOT}/src/files/.htaccess
